d8591e70df9ad4e2cf5218e6e8439512ad8763dc
Marc <marc@marmar.io>
2020-01-01 15:03:33 -0800

    fixed filtering

--- src/MovieFilters.js
+++ src/MovieFilters.js
@@ -5,15 +5,11 @@ import DecadeFilter from "./DecadeFilter";
 /** @type {React.FC<{movies: import("./Movies").MovieInfo[], updateMovies: (import("./Movies").MovieInfo[]) => void}} */
 const MovieFilters = ({ movies, updateMovies }) => {
   const [searchFilteredMovies, updateSearchFilteredMovies] = useState(movies);
-  console.log("search list", searchFilteredMovies);
+  
   const [decadeFilteredMovies, updateDecadeFilteredMovies] = useState(
     searchFilteredMovies
   );
-  console.log("decade list", decadeFilteredMovies);
 
-  useEffect(() => updateDecadeFilteredMovies(searchFilteredMovies), [
-    searchFilteredMovies
-  ]);
   useEffect(() => updateMovies(decadeFilteredMovies), [decadeFilteredMovies]);
 
   return (
